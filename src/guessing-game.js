12173c4ceadb645eaa79eaa48f930938aff75e5e
FeuerImBlut <golubovich.as@gmail.com>
2021-09-02 10:41:10 +0300

    init add implementayion

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+		this.min = min;
+		this.max = max;
+		this.mid = 1;
     }
 
     guess() {
-
+		this.mid = Math.round((this.min + this.max)/2);
+		return this.mid;
     }
 
     lower() {
+		this.max = this.mid;
+		
 
     }
 
     greater() {
-
+		this.min = this.mid;
     }
 }
 
